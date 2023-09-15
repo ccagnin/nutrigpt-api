2313c4cbb7fc3f527ff1db81b977da7496e5ce90
Caroline Cagnin <cagnin.caroline@gmail.com>
2023-09-15 15:50:35 -0300

    add: user mesuares module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MeasuresModule } from './measures/measures.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, MeasuresModule],
 })
 export class AppModule {}
