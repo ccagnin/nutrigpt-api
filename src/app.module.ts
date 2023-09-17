b661c42b402fe7b414a1328f4eed9d38626a5e62
Caroline Cagnin <cagnin.caroline@gmail.com>
2023-09-17 15:56:19 -0300

    add: database and docker-compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MeasuresModule } from './measures/measures.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, MeasuresModule],
+  imports: [AuthModule, UserModule, MeasuresModule, PrismaModule],
 })
 export class AppModule {}
