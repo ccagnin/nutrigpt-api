0088553c465d15fdef8155d396c3452f3b73815c
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-01-04 18:55:48 -0300

    change mp key

--- src/payment/mercado-pago-config.service.ts
+++ src/payment/mercado-pago-config.service.ts
@@ -5,8 +5,7 @@ import { MercadoPagoConfig } from 'mercadopago';
 export class MercadoPagoConfigService {
   getConfig(): MercadoPagoConfig {
     return {
-      accessToken:
-        'APP_USR-8885514618504695-101908-f637f5960b2189a30368496fb726c871-20177416',
+      accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN || '',
     };
   }
 }
